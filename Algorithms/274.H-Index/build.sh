262f2d27d31dfca6a063e409d4d70e1d354f2f4a
liuchu01 <lc@BaiduMac.lan>
2016-03-05 23:19:17 +0800

    build.sh

--- Algorithms/274.H-Index/build.sh
+++ Algorithms/274.H-Index/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-gcc answer.c -DDEBUG -Wall -g -o debug
+# specify for GNU gdb
+#gcc answer.c -DDEBUG -Wall -ggdb -o debug
+# nomal
+#gcc answer.c -DDEBUG -Wall -g -o debug
+# MAC have no gdb :-(
+gcc answer.c -DDEBUG -Wall -o debug
 if [ $? -eq 0 ]; then
     gcc answer.c -O3 -o release
 fi
