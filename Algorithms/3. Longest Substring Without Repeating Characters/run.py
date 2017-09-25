4283b1ebb0157973a8f70a9d6ba82f9416208a54
LawrenceLiu <liuchu1.0@gmail.com>
2017-09-25 23:32:00 +0800

    version 2.0

--- Algorithms/3. Longest Substring Without Repeating Characters/run.py
+++ Algorithms/3. Longest Substring Without Repeating Characters/run.py
@@ -17,23 +17,23 @@ class Solution(object):
         if "" == s:
             return 0
         total = len(s)
-        max_sub = s[0]
-        max_len = len(max_sub)
+        # max_sub = s[0]
+        max_len = 1
         last_pos = {s[0]:0}
         i, j = 0, 1
-        last_str = max_sub
+        last_str = s[0]
         while j < total:
             if s[j] in last_str:
                 i = last_pos[s[j]] + 1
             last_pos[s[j]] = j
             last_str = s[i:j+1]
-            last_len = len(last_str)
-            if len(set(last_str)) == last_len and last_len >= max_len:
-                max_sub = last_str
+            last_len = j-i+1
+            if last_len >= max_len:
+                # max_sub = last_str
                 max_len = last_len
             j +=1
-        print(max_sub)
-        return len(max_sub)
+        # print(max_sub)
+        return max_len
 
 if __name__ == "__main__":
     main = Solution()
