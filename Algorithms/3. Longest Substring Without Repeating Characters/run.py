dbc5dbdfdabdccb315b5dccb3e4f3dd62ded26e7
LawrenceLiu <liuchu1.0@gmail.com>
2017-09-25 23:10:39 +0800

    version 1.0

--- Algorithms/3. Longest Substring Without Repeating Characters/run.py
+++ Algorithms/3. Longest Substring Without Repeating Characters/run.py
@@ -14,10 +14,30 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        print(s)
+        if "" == s:
+            return 0
+        total = len(s)
+        max_sub = s[0]
+        max_len = len(max_sub)
+        last_pos = {s[0]:0}
+        i, j = 0, 1
+        last_str = max_sub
+        while j < total:
+            if s[j] in last_str:
+                i = last_pos[s[j]] + 1
+            last_pos[s[j]] = j
+            last_str = s[i:j+1]
+            last_len = len(last_str)
+            if len(set(last_str)) == last_len and last_len >= max_len:
+                max_sub = last_str
+                max_len = last_len
+            j +=1
+        print(max_sub)
+        return len(max_sub)
 
 if __name__ == "__main__":
     main = Solution()
-    main.lengthOfLongestSubstring("abcabcbb")
-    main.lengthOfLongestSubstring("bbbbb")
-    main.lengthOfLongestSubstring("pwwkew")
+    print(main.lengthOfLongestSubstring("ggububgvfk"))
+    print(main.lengthOfLongestSubstring("abcabcbb"))
+    print(main.lengthOfLongestSubstring("bbbbb"))
+    print(main.lengthOfLongestSubstring("pwwkew"))
